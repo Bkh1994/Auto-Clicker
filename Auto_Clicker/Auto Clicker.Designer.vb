029921c1a41cccbde07b4cbed728c22c62d9e464
Brandon Harris <bharris@ajaxring.pri>
2019-04-09 09:52:45 -0400

    Add project files.

---
+++ Auto_Clicker/Auto Clicker.Designer.vb
@@ -0,0 +1,267 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.minTime = New System.Windows.Forms.TextBox()
+        Me.maxTime = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.startButton = New System.Windows.Forms.Button()
+        Me.stopButton = New System.Windows.Forms.Button()
+        Me.startKey = New System.Windows.Forms.TextBox()
+        Me.stopKey = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.startTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.stopTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.submitKeys = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.errorLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'minTime
+        '
+        Me.minTime.Location = New System.Drawing.Point(81, 7)
+        Me.minTime.Name = "minTime"
+        Me.minTime.Size = New System.Drawing.Size(78, 20)
+        Me.minTime.TabIndex = 1
+        Me.minTime.Text = "100"
+        Me.minTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'maxTime
+        '
+        Me.maxTime.Location = New System.Drawing.Point(81, 34)
+        Me.maxTime.Name = "maxTime"
+        Me.maxTime.Size = New System.Drawing.Size(78, 20)
+        Me.maxTime.TabIndex = 3
+        Me.maxTime.Text = "100"
+        Me.maxTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "AT LEAST"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "AT MOST"
+        '
+        'startButton
+        '
+        Me.startButton.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.startButton.Location = New System.Drawing.Point(12, 165)
+        Me.startButton.Name = "startButton"
+        Me.startButton.Size = New System.Drawing.Size(116, 43)
+        Me.startButton.TabIndex = 6
+        Me.startButton.Text = "START"
+        Me.startButton.UseVisualStyleBackColor = True
+        '
+        'stopButton
+        '
+        Me.stopButton.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.stopButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.stopButton.Location = New System.Drawing.Point(156, 165)
+        Me.stopButton.Name = "stopButton"
+        Me.stopButton.Size = New System.Drawing.Size(116, 43)
+        Me.stopButton.TabIndex = 7
+        Me.stopButton.Text = "STOP"
+        Me.stopButton.UseVisualStyleBackColor = True
+        '
+        'startKey
+        '
+        Me.startKey.AccessibleName = "startKey"
+        Me.startKey.Location = New System.Drawing.Point(13, 111)
+        Me.startKey.Name = "startKey"
+        Me.startKey.Size = New System.Drawing.Size(115, 20)
+        Me.startKey.TabIndex = 8
+        Me.startKey.Text = "a"
+        '
+        'stopKey
+        '
+        Me.stopKey.AccessibleName = "stopKey"
+        Me.stopKey.Location = New System.Drawing.Point(156, 110)
+        Me.stopKey.Name = "stopKey"
+        Me.stopKey.Size = New System.Drawing.Size(116, 20)
+        Me.stopKey.TabIndex = 9
+        Me.stopKey.Text = "z"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(165, 10)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "MILLISECONDS"
+        '
+        'startTimer
+        '
+        Me.startTimer.Interval = 1
+        '
+        'stopTimer
+        '
+        Me.stopTimer.Interval = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(23, 214)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 13
+        Me.TextBox1.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(168, 214)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 14
+        Me.TextBox2.Visible = False
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 1
+        '
+        'submitKeys
+        '
+        Me.submitKeys.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.submitKeys.Location = New System.Drawing.Point(12, 136)
+        Me.submitKeys.Name = "submitKeys"
+        Me.submitKeys.Size = New System.Drawing.Size(260, 23)
+        Me.submitKeys.TabIndex = 15
+        Me.submitKeys.Text = "Submit Start/Stop Buttons"
+        Me.submitKeys.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(165, 37)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 13)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "MILLISECONDS"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(20, 65)
+        Me.Label6.MaximumSize = New System.Drawing.Size(116, 43)
+        Me.Label6.MinimumSize = New System.Drawing.Size(116, 43)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(116, 43)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Enter your starting button below:               (Letters or numbers)"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Label7.Location = New System.Drawing.Point(165, 64)
+        Me.Label7.MaximumSize = New System.Drawing.Size(116, 43)
+        Me.Label7.MinimumSize = New System.Drawing.Size(116, 43)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(116, 43)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Enter your stopping button below:               (Letters or numbers)"
+        '
+        'errorLabel
+        '
+        Me.errorLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.errorLabel.ForeColor = System.Drawing.Color.Red
+        Me.errorLabel.Location = New System.Drawing.Point(10, 217)
+        Me.errorLabel.Name = "errorLabel"
+        Me.errorLabel.Size = New System.Drawing.Size(258, 57)
+        Me.errorLabel.TabIndex = 21
+        Me.errorLabel.Text = "Label3"
+        Me.errorLabel.Visible = False
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.submitKeys
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.CancelButton = Me.stopButton
+        Me.ClientSize = New System.Drawing.Size(284, 218)
+        Me.Controls.Add(Me.errorLabel)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.submitKeys)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.stopKey)
+        Me.Controls.Add(Me.startKey)
+        Me.Controls.Add(Me.stopButton)
+        Me.Controls.Add(Me.startButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.maxTime)
+        Me.Controls.Add(Me.minTime)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents minTime As TextBox
+    Friend WithEvents maxTime As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents startButton As Button
+    Friend WithEvents stopButton As Button
+    Friend WithEvents startKey As TextBox
+    Friend WithEvents stopKey As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents startTimer As Timer
+    Friend WithEvents stopTimer As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents submitKeys As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents errorLabel As System.Windows.Forms.Label
+End Class
---
+++ Auto_Clicker/Auto Clicker.vb
@@ -0,0 +1,382 @@
+﻿Public Class Form1
+    Public Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As Integer)
+
+    Public startingKey As Integer = 0 'Keys to start/stop the autoclicker
+    Public stoppingKey As Integer = 0
+
+    Dim maxLength As Integer = 10 'The max length of the integer textboxes (min/max timer)
+
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+    Private Sub startButton_Click(sender As Object, e As EventArgs) Handles startButton.Click
+        startTimer.Enabled = True
+        Randomize()
+        Dim nextTimer As Integer = CInt(Int((maxTime.Text * Rnd()) + minTime.Text))
+        startTimer.Interval = nextTimer
+    End Sub
+
+    Private Sub stopButton_Click(sender As Object, e As EventArgs) Handles stopButton.Click
+        startTimer.Enabled = False
+    End Sub
+
+    Private Sub startTimer_Tick(sender As Object, e As EventArgs) Handles startTimer.Tick
+        Dim position
+        position = Cursor.Position
+        Cursor.Position = New Point(Cursor.Position.X, Cursor.Position.Y)
+        mouse_event(&H2, 0, 0, 0, 0) 'Left mouse key down
+        mouse_event(&H4, 0, 0, 0, 0) 'Left mouse key let up
+        Me.Activate()
+    End Sub
+
+    Private Sub submitKeys_Click(sender As Object, e As EventArgs) Handles submitKeys.Click
+
+        startButton.Text = "START"
+        stopButton.Text = "STOP"
+
+        If startKey.Text = "a" Then 'Gets the key that the user wants to start the autoclicker with
+            startingKey = 97
+
+        ElseIf startKey.Text = "b" Then
+            startingKey = 98
+
+        ElseIf startKey.Text = "c" Then
+            startingKey = 99
+
+        ElseIf startKey.Text = "d" Then
+            startingKey = 100
+
+        ElseIf startKey.Text = "e" Then
+            startingKey = 101
+
+        ElseIf startKey.Text = "f" Then
+            startingKey = 102
+
+        ElseIf startKey.Text = "g" Then
+            startingKey = 103
+
+        ElseIf startKey.Text = "h" Then
+            startingKey = 104
+
+        ElseIf startKey.Text = "i" Then
+            startingKey = 105
+
+        ElseIf startKey.Text = "j" Then
+            startingKey = 106
+
+        ElseIf startKey.Text = "k" Then
+            startingKey = 107
+
+        ElseIf startKey.Text = "l" Then
+            startingKey = 108
+
+        ElseIf startKey.Text = "m" Then
+            startingKey = 109
+
+        ElseIf startKey.Text = "n" Then
+            startingKey = 110
+
+        ElseIf startKey.Text = "o" Then
+            startingKey = 111
+
+        ElseIf startKey.Text = "p" Then
+            startingKey = 112
+
+        ElseIf startKey.Text = "q" Then
+            startingKey = 113
+
+        ElseIf startKey.Text = "r" Then
+            startingKey = 114
+
+        ElseIf startKey.Text = "s" Then
+            startingKey = 115
+
+        ElseIf startKey.Text = "t" Then
+            startingKey = 116
+
+        ElseIf startKey.Text = "u" Then
+            startingKey = 117
+
+        ElseIf startKey.Text = "v" Then
+            startingKey = 118
+
+        ElseIf startKey.Text = "w" Then
+            startingKey = 119
+
+        ElseIf startKey.Text = "x" Then
+            startingKey = 120
+
+        ElseIf startKey.Text = "y" Then
+            startingKey = 121
+
+        ElseIf startKey.Text = "z" Then
+            startingKey = 122
+
+        ElseIf startKey.Text = "0" Then
+            startingKey = 48
+
+        ElseIf startKey.Text = "1" Then
+            startingKey = 49
+
+        ElseIf startKey.Text = "2" Then
+            startingKey = 50
+
+        ElseIf startKey.Text = "3" Then
+            startingKey = 51
+
+        ElseIf startKey.Text = "4" Then
+            startingKey = 52
+
+        ElseIf startKey.Text = "5" Then
+            startingKey = 53
+
+        ElseIf startKey.Text = "6" Then
+            startingKey = 54
+
+        ElseIf startKey.Text = "7" Then
+            startingKey = 55
+
+        ElseIf startKey.Text = "8" Then
+            startingKey = 56
+
+        ElseIf startKey.Text = "9" Then
+            startingKey = 57
+
+        Else
+            startingKey = 97
+        End If
+
+        If stopKey.Text = "a" Then 'Gets the key that the user wants to stop the autoclicker with
+            stoppingKey = 97
+
+        ElseIf stopKey.Text = "b" Then
+            stoppingKey = 98
+
+        ElseIf stopKey.Text = "c" Then
+            stoppingKey = 99
+
+        ElseIf stopKey.Text = "d" Then
+            stoppingKey = 100
+
+        ElseIf stopKey.Text = "e" Then
+            stoppingKey = 101
+
+        ElseIf stopKey.Text = "f" Then
+            stoppingKey = 102
+
+        ElseIf stopKey.Text = "g" Then
+            stoppingKey = 103
+
+        ElseIf stopKey.Text = "h" Then
+            stoppingKey = 104
+
+        ElseIf stopKey.Text = "i" Then
+            stoppingKey = 105
+
+        ElseIf stopKey.Text = "j" Then
+            stoppingKey = 106
+
+        ElseIf stopKey.Text = "k" Then
+            stoppingKey = 107
+
+        ElseIf stopKey.Text = "m" Then
+            stoppingKey = 109
+
+        ElseIf stopKey.Text = "n" Then
+            stoppingKey = 110
+
+        ElseIf stopKey.Text = "l" Then
+            stoppingKey = 108
+
+        ElseIf stopKey.Text = "o" Then
+            stoppingKey = 111
+
+        ElseIf stopKey.Text = "p" Then
+            stoppingKey = 112
+
+        ElseIf stopKey.Text = "q" Then
+            stoppingKey = 113
+
+        ElseIf stopKey.Text = "r" Then
+            stoppingKey = 114
+
+        ElseIf stopKey.Text = "s" Then
+            stoppingKey = 115
+
+        ElseIf stopKey.Text = "t" Then
+            stoppingKey = 116
+
+        ElseIf stopKey.Text = "u" Then
+            stoppingKey = 117
+
+        ElseIf stopKey.Text = "v" Then
+            stoppingKey = 118
+
+        ElseIf stopKey.Text = "w" Then
+            stoppingKey = 119
+
+        ElseIf stopKey.Text = "x" Then
+            stoppingKey = 120
+
+        ElseIf stopKey.Text = "y" Then
+            stoppingKey = 121
+
+        ElseIf stopKey.Text = "z" Then
+            stoppingKey = 122
+
+        ElseIf stopKey.Text = "0" Then
+            stoppingKey = 48
+
+        ElseIf stopKey.Text = "1" Then
+            stoppingKey = 49
+
+        ElseIf stopKey.Text = "2" Then
+            stoppingKey = 50
+
+        ElseIf stopKey.Text = "3" Then
+            stoppingKey = 51
+
+        ElseIf stopKey.Text = "4" Then
+            stoppingKey = 52
+
+        ElseIf stopKey.Text = "5" Then
+            stoppingKey = 53
+
+        ElseIf stopKey.Text = "6" Then
+            stoppingKey = 54
+
+        ElseIf stopKey.Text = "7" Then
+            stoppingKey = 55
+
+        ElseIf stopKey.Text = "8" Then
+            stoppingKey = 56
+
+        ElseIf stopKey.Text = "9" Then
+            stoppingKey = 57
+
+        Else
+            stoppingKey = 98
+        End If
+
+        startButton.Text = startButton.Text + " (" + startKey.Text + ")"
+        stopButton.Text = stopButton.Text + " (" + stopKey.Text + ")"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "AUTO CLICKER"
+        Dim ico As New System.Drawing.Icon("AC Icon.ico.ico")
+        Me.Icon = ico
+        submitKeys.Select()
+        minTime.BackColor = DefaultBackColor
+        startingKey = 0
+        stoppingKey = 0
+        submitKeys_Click(Me, EventArgs.Empty) 'Sets the start/stop keys before starting
+    End Sub
+
+    Private Sub Form1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar = ChrW(startingKey) Then 'Starts the autoclicker if the starting key is pressed
+            Call startButton_Click(Me, EventArgs.Empty)
+        End If
+        If e.KeyChar = ChrW(stoppingKey) Then 'Stops the autoclicker if the stopping key is pressed
+            Call stopButton_Click(Me, EventArgs.Empty)
+        End If
+    End Sub
+
+    Private Sub minTime_TextChanged(sender As Object, e As EventArgs) Handles minTime.TextChanged
+        Dim dummy As Double
+        Dim dummyInt As Integer
+
+        If Not Double.TryParse(minTime.Text, dummy) Then
+            Me.Height = 285
+            errorLabel.Text = "Error: The minimum time must be a number."
+            errorLabel.Visible = True
+            minTime.BackColor = Color.Red
+
+        Else If Not Integer.TryParse(minTime.Text, dummyInt) Then
+            Me.Height = 285
+            errorLabel.Text = "Error: The minimum time is too large."
+            errorLabel.Visible = True
+            minTime.BackColor = Color.Red
+
+
+        ElseIf minTime.Text > maxTime.Text Then
+            Me.Height = 285
+            errorLabel.Text = "Error: The minimum time cannot exceed the maximum time."
+            errorLabel.Visible = True
+            minTime.BackColor = Color.Red
+
+        Else
+            Me.Height = 256
+            minTime.BackColor = DefaultBackColor
+            errorLabel.Visible = False
+        End If
+    End Sub
+
+    Private Sub maxTime_TextChanged(sender As Object, e As EventArgs) Handles maxTime.TextChanged
+        Dim dummy As Double
+
+        If Not Double.TryParse(maxTime.Text, dummy) Then
+            Me.Height = 285
+            errorLabel.Text = "Error: The maximum time must be a number."
+            errorLabel.Visible = True
+            maxTime.BackColor = Color.Red
+
+        ElseIf (maxTime.Text.Length >= maxLength) Then
+            Me.Height = 285
+            errorLabel.Text = "Error: The maximum time is too large."
+            errorLabel.Visible = True
+            maxTime.BackColor = Color.Red
+
+        ElseIf minTime.Text > maxTime.Text Then
+            Me.Height = 285
+            errorLabel.Text = "Error: The minimum time cannot exceed the maximum time."
+            errorLabel.Visible = True
+            minTime.BackColor = Color.Red
+
+        ElseIf minTime.Text <= maxTime.Text Then
+            Me.Height = 256
+            minTime.BackColor = DefaultBackColor
+            errorLabel.Visible = False
+
+        Else
+        Me.Height = 256
+        maxTime.BackColor = DefaultBackColor
+        errorLabel.Visible = False
+        End If
+    End Sub
+
+    Private Sub startKey_TextChanged(sender As Object, e As EventArgs) Handles startKey.TextChanged
+        Dim dummy As Char
+
+        If Not Char.TryParse(startKey.Text, dummy) Then
+            Me.Height = 303
+            errorLabel.Text = "Error: The starting button must be a single key."
+            errorLabel.Visible = True
+            startKey.BackColor = Color.Red
+            Return
+        End If
+
+        If Char.TryParse(startKey.Text, dummy) Then
+            Me.Height = 256
+            startKey.BackColor = DefaultBackColor
+            errorLabel.Visible = False
+        End If
+    End Sub
+
+    Private Sub stopKey_TextChanged(sender As Object, e As EventArgs) Handles stopKey.TextChanged
+        Dim dummy As Char
+
+        If Not Char.TryParse(stopKey.Text, dummy) Then
+            Me.Height = 303
+            errorLabel.Text = "Error: The stopping button must be a single key."
+            errorLabel.Visible = True
+            stopKey.BackColor = Color.Red
+            Return
+        End If
+
+        If Char.TryParse(stopKey.Text, dummy) Then
+            Me.Height = 256
+            stopKey.BackColor = DefaultBackColor
+            errorLabel.Visible = False
+        End If
+    End Sub
+End Class
